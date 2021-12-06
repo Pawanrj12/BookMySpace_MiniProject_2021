dff9a215b6334a87283bfdd898c93efcae1e99ee
Pawanrj12 <82582124+Pawanrj12@users.noreply.github.com>
2021-12-06 20:09:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,713 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>BookMySpace</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/pp.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span>
+
+</span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">Book<span class="themecolor"> MY</span>Space</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+                                            
+<li><a href="#one" class="page-scroll"><h3>About</h3></a></li>
+                                
+<li><a href="#four" class="page-scroll"><h3>Contact</h3></a></li>
+
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" GET EVENTS, GET EVENTS, GET EVENTS"></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" TICKETS ONLINE, TICKETS ONLINE, TICKETS ONLINE"></span></h1>
+       
+<p class="main-heading-text">BookMySpace Team,<br/> Welcomes You</p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Book Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">We are Creative</h1>
+        
+<p class="main-heading-text">We are Welcomes you to BookMySpace this is our Events Booking and Ticket Booking Website.</p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Ge Started</a>
+          
+<a href="#" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+<section class="aboutus white-background black" id="one">
+     
+<div class="container">
+         
+<div class="row">
+             
+<div class="col-md-12 text-center black">
+                 
+<h3 class="title">ABOUT <span class="themecolor">US</span></h3>
+            
+<p class="a-slog">The project entitled “BookMySpace” was developed using HTML and CSS as Front end and PHP as Backend and payment gateway for payment purposes. BookMySpace is useful for the volunteers who are facing problems while selling the tickets or promoting their upcoming events. It will not only help the volunteers, but it also helps the users who are part of any club to be aware of the events that are going to be held. Registration through our website helps in managing the memberships. Participants can register themselves, buy tickets online, can check the status. Registration can be done through online transactions. In the manual system, the volunteers have to sell tickets by going from place to place in the college. This online system will help save their time. Initially, the user needs to register with the application by submitting their details online. The administrator will verify the user’s username and password. The registration process is carried by paying the money using the online transaction</p> 
+             
+</div>
+         
+</div>
+         
+<div class="gap">
+             
+         
+</div>
+         
+         
+<div class="row about-box">
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-newspaper-o"></i>
+              
+<h4>REGISTER YOUR EVENTS</h4>
+              
+
+            
+</div> <!-- / margin -->
+          
+</div> <!-- /col -->
+          
+<div class="col-sm-4 about-line text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<h4>FULLY SUPPORTIVE</h4>
+              
+
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+          
+<div class="col-sm-4 text-center">
+            
+<div class="margin-bottom">
+              
+<i class="fa fa-area-chart"></i>
+              
+<h4>BOOK & CANCEL ANYTIME</h4>
+              
+
+            
+</div> <!-- / margin -->
+          
+</div><!-- /col -->
+        
+</div> <!-- /row -->
+         
+         
+         
+         
+     
+</div>
+ </section>
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Enjoy?</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>inspire your customer here!</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/INSPIRATION]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<section class="services white-background black" id="seven">
+     
+ <div class="container">
+        
+<div class="row text-center">
+          
+<div class="col-md-12">
+              
+<h3 class="title">We Are <span class="themecolor">Good In</span></h3>
+            
+<p class="a-slog"></p>
+          
+</div> <!-- /col -->
+        
+</div> <!-- /row -->
+        
+<div class="gap"></div>
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-diamond"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Strategy Solutions</h4>
+                
+<p>Our event registration system helps you increase response rates without the headaches associated with spreadsheets and paper forms. Manage invitees, registrants, and even multiple events.</p>
+   
+           </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-tablet">
+
+ </i>
+             
+ <div class="nim-service-detail">
+              
+  <h4>Server Support</h4>
+              
+  <p>BookMySpace is a provide you 24X7 help Support</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+              
+<i class="fa fa-magic"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>SEO</h4>
+                
+<p>Your events are unique. You need a registration system that can be tailored to your specific needs. Our rich feature set makes your small meetings easy and scales up to handle the largest user conferences.</p>
+ 
+             </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->       
+        
+</div> <!-- end row -->
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-rocket"></i>
+             
+ <div class="nim-service-detail">
+               
+ <h4>Graphic Design</h4>
+                
+<p>Quickly design simple events to save time. From website to emails, your fully branded event will look and feel like your event, not ours.</p>
+          
+    </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+         
+ </div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+           
+ <div class="nim-service margin-bottom">
+              
+<i class="fa fa-map-marker"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Analystics</h4>
+                
+<p>Build a dynamic, multi-track, multi-session online event registration process.</p>
+             
+ </div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-paypal"></i>
+              
+<div class="nim-service-detail">
+               
+ <h4>Payment Support</h4>
+                
+<p>BookMySpace Accepts all type of Payment  Domestic and International Credit & Debit cards, EMIs ( Credit/Debit Cards & Cardless), PayLater, Netbanking from 58 banks, UPI and 8 mobile wallets</p>
+              
+</div> <!-- /nim-service-detail -->
+            
+</div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->         
+        
+</div> <!-- end row -->
+
+        
+<div class="row">
+          
+<div class="col-sm-4">
+            
+<div class="nim-service margin-bottom">
+             
+ <i class="fa fa-bar-chart-o"></i>
+              
+<div class="nim-service-detail">
+                
+<h4>Analystics</h4>
+            
+ <p>BookMySpace software solution lets your attendees enter their own information that you can oversee and extract.Its maintain all your data Regarding Payment & Booking </p>
+             
+ </div> <!-- /nim-service-detail -->
+           
+ </div> <!-- /nim-service margin-bottom -->
+          
+</div> <!-- /col -->
+
+               
+       
+ </div> <!-- end row -->
+
+      
+</div>  <!-- container -->
+    
+</section>
+ 
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+<!--sub-form-->
+<section class="sub-form text-center" id="eight">
+  
+<div class="container">
+    <div class="col-md-12">
+        
+<h3 class="title">Subscribe to our <span class="themecolor"> News letter</span></h3>
+            
+<p class="lead">Drop You Email for feature Query.We will Contact you soon</p>
+    
+</div> 
+    
+<div class="row">
+        
+<div class="col-md-3 col-sm-3"></div>
+      
+<div class="col-md-6 center-block col-sm-6 ">
+        
+<form id="mc-form">
+          
+<div class="input-group">
+            
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+           
+<span class="input-group-btn">
+            
+<button type="submit" class="btn btn-default">SUBSCRIBE <i class="fa fa-envelope"></i> </button>
+            
+</span> </div>
+          
+<label for="mc-email" id="mc-notification"></label>
+       
+ </form>
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</section>
+
+<!--sub-form end--> 
+
+
+ 
+ 
+<!-- [/CONTACT]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small> 2021, Brought To You By <a href="">BookMySpace</a> </small></div>
+      
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+          
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          
+<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
